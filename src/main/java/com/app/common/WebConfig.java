ad5749a067855370c84c5e7c528f0490c5731a49
DESKTOP-TFLB83F\dlwng <dlwngud6333@naver.com>
2020-12-12 18:50:32 +0900

    xss Filter 적용

--- src/main/java/com/app/common/WebConfig.java
+++ src/main/java/com/app/common/WebConfig.java
@@ -2,7 +2,6 @@ package com.app.common;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
--- src/main/java/com/app/common/XSSFilter.java
+++ src/main/java/com/app/common/XSSFilter.java
@@ -9,36 +9,41 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * @author dlwng
+ *  lucy 필터는 form data 전송 방식엔 유효하지만, @RequestBody로 전달되는 JSON 요청은 처리해주지 않는다
+ *  RequestWrapper 추가하여 RequestBody로 전달되는 파라미터에도 적용
+ *
+ */
 @Slf4j
 @WebFilter(urlPatterns= "/*", filterName = "XSSFilter")
-public class XSSFilter implements Filter{
-
-	
-	
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
-		Filter.super.init(filterConfig);
-	}
+public class XSSFilter implements Filter {
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		chain.doFilter(request, response);
-
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		RequestWrapper requestWrapper = null;
+
+		try {
+			requestWrapper = new RequestWrapper(request);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		chain.doFilter(requestWrapper, response);
 	}
 
 	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-		Filter.super.destroy();
-	}
+	public void init(FilterConfig filterConfig) throws ServletException {}
 
-	
-	
+	@Override
+	public void destroy() {}
+
 }
